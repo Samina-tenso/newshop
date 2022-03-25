09d2da30d40bf8c0874562b322d1150b179d907c
Samina-tenso <tensammi@gmail.com>
2022-03-25 21:42:57 +0100

    removed webshop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./index.css"
-import Cart from "./components/Cart"
-import ProductItem from "./components/ProductItem"
-import Products from "./components/Products.js"
+import Cart from "./Cart"
+import ProductItem from "./ProductItem"
+import Products from "./Products.js"
 import React, { Suspense } from "react"
 import { Routes, Route, Link } from "react-router-dom"
 
@@ -11,8 +11,8 @@ export default function App() {
   return (
 
     <div className="App">
-      <Link to="/webshop">Products </Link>
-      <Link to="/webshop/cart">Cart</Link>
+      <Link to="/">Products </Link>
+      <Link to="/cart">Cart</Link>
       <Suspense fallback={<div> Loading...</div>}>
 
         <Routes>
